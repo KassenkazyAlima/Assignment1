80e7782464b03ebbd076fd24924eb70fd1a9690c
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-01-16 13:00:13 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,31 @@ import package1.Shape;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // Create points
-        Point point1 = new Point(1.0, 2.0);
-        Point point2 = new Point(1.0, 2.0);
-        Point point3 = new Point(1.0, 3.0);
-        Point point4 = new Point(1.0, 4.0);
-        Point point5 = new Point(1.0, 5.0);
-        Point point6 = new Point(2.0, 1.0);
-        Point point7 = new Point(2.0, 2.0);
-        Point point8 = new Point(2.0, 3.0);
-        Point point9 = new Point(2.0, 4.0);
-        Point point10 = new Point(2.0, 5.0);
+        try {
+            File file = new File("/Users/alimakairat/IdeaProjects/MyFirstProject/src");
 
-        // Create an ArrayList of points
-        ArrayList<Point> pointsList = new ArrayList<>();
-        pointsList.add(point1);
-        pointsList.add(point2);
-        pointsList.add(point3);
-        pointsList.add(point4);
-        pointsList.add(point5);
-        pointsList.add(point6);
-        pointsList.add(point7);
-        pointsList.add(point8);
-        pointsList.add(point9);
-        pointsList.add(point10);
-
-        // Create a Shape object with the ArrayList of points
-        Shape shape = new Shape(pointsList);
-
-        // Calculate and print the perimeter, longest side, and average side
-        System.out.println(shape.calculatePerimeter());
-        System.out.println(shape.getLongestSide());
+            Scanner sc = new Scanner(file);
+            Shape shape = new Shape();
+            while (sc.hasNext()) {
+                double x = sc.nextDouble();
+                double y = sc.nextDouble();
+                Point p = new Point(x, y);
+                shape.addPoint(p);
+            }
+            for (int i = 0; i < shape.getNumberOfPoints(); i++) {
+                Point point1 = shape.getPoint(i);
+                Point point2 = shape.getPoint((i + 1) % shape.getNumberOfPoints());
+                double distance = point1.distance(point2);
+                System.out.println("Distance between " + point1 + " and " + point2 + ": " + distance);
+            }
+            System.out.println("Perimeter " + shape.calculatePerimeter());
+            System.out.println("Longest side " + shape.getLongestSide());
+            System.out.println("Average side " + shape.getAverageSide());
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found:(");
+        }
     }
 }

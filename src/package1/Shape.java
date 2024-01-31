7818a6b7c99d442fe6ab37a35b81a9fd7c6ccd10
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-01-31 22:34:25 +0600

    Shape.java

--- src/package1/Shape.java
+++ src/package1/Shape.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Shape {
     private ArrayList<Point> points; // container of Points
 
-    public Shape(ArrayList<Point> points) {
-        this.points = points;
+    public Shape() {
+        this.points = new ArrayList<>();
     }
+
     public void addPoint(Point point) {
         points.add(point);
     }
@@ -26,15 +27,10 @@ public class Shape {
     }
 
     public double getAverageSide(){
-        double averageSide = 0;
-        for(int i =0;i<points.size();i++){
-            Point currentPoint = points.get(i);
-            Point nextPoint = points.get((i+1) % points.size());
+        double totalDistance = calculatePerimeter();
+        int size = points.size();
+        return totalDistance / size;
 
-            averageSide += currentPoint.getDistance(nextPoint);
-        }
-
-        return averageSide/points.size();
     }
 
     public double getLongestSide() {
@@ -53,5 +49,16 @@ public class Shape {
         }
         return longestSide;
     }
+    public int getNumberOfPoints() {
+        return points.size();
+    }
+
+    public Point getPoint(int i) {
+        if (i >= 0 && i < points.size()) {
+            return points.get(i);
+        }
+        return null;
+    }
+
 
 }
